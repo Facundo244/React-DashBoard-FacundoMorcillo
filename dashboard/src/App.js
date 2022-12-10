6abba8d0148fb4687469942dbad64e422e350fbf
Facundo244 <facaagus@gmail.com>
2022-12-10 06:38:26 -0300

    primera parte react

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SideBar from './components/SideBar';
+import './assets/css/app.css';
+
+function App() {
+  return (
+    <div id="wrapper">
+      <SideBar/>
+    </div>
+  );
+}
+
+export default App;
